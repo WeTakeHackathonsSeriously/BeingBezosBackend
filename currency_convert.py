f6eebc42d2aba944e2807894fb93221bb97ebdf7
Aporva Varshney <av918@ic.ac.uk>
2020-02-09 02:20:22 +0000

    Added currency conversion

--- currency_convert.py
+++ currency_convert.py
@@ -1,13 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from forex_python.converter import CurrencyRates
 
-def get_conversion_rate():
-    url = 'https://www.google.com/search?q="1 usd to gbp"'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    pound = soup.findAll("span", {"class": "DFlfde SwHCTb"})[0]['data-value']
-    return float(pound)
-
-if __name__ == '__main__':
-    print(get_conversion_rate())
+def one_dollar_is():
+    return CurrencyRates().get_rate('USD', 'GBP')
 
